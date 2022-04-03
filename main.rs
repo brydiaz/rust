aab15d3594badf47958e5ab1e6993816f2ff6233
brydiaz <bryandiaz13301@gmail.com>
2022-04-03 17:01:47 -0600

    Opcion 1 lista

--- main.rs
+++ main.rs
@@ -46,22 +46,55 @@ fn main() {
 fn read_syscalls(program:&str, arguments:Vec<String>){
     let mut tracer;
     if arguments.len() == 0{
-        println!("tiene 0");
         tracer = HStraceBuilder::new().program(program).build();//Si no tiene argumentos no los configura
     }else {
-        println!("no tiene 0");
         tracer = HStraceBuilder::new().program(program).args(arguments).build();
     }
 
     tracer.start().unwrap();//Inicia el tracer
+    let mut syscalls = Vec::<String>::new();
+    let mut syscalls_list = Vec::<(String, i32)>::new();
     for syscall in tracer.iter_as_syscall() {
-        match syscall.name {
-            hstrace::Ident::Openat | hstrace::Ident::Fstat | hstrace::Ident::Stat => {
-                println!("");
-                println!("File operation detected: {:?}", syscall);
-            }
-            _ => (),
+        //println!("File operation detected: {:?}", syscall);
+        if is_in(syscall.name.to_string(), &syscalls) == false{
+            syscalls_list.push((syscall.name.to_string(),1));
+            syscalls.push(syscall.name.to_string());
+        } else {
+            act_index(syscall.name.to_string(), &mut syscalls_list)   
+        }
+      
+    }
+
+    println!("{:?}","");
+    print_table_array(&mut syscalls_list);
+    println!("{:?}","");
+
+}
+
+//Verfica si el elemento está en el vector
+fn is_in(element:String,vector:&Vec<String>)->bool{
+    for i in vector{
+        if i.to_string() == element{
+            return true;
         }
     }
-    
+    return false;
+}
+
+//Actualiza en uno el elemento que haya en el vector
+fn act_index(element:String,vector: &mut Vec<(String, i32)>){
+    for  i in vector.iter_mut(){
+        if i.0.to_string() == element{
+            i.1 += 1
+        }
+    }
+
+}
+
+//Imprime bonito un arreglo de tuplas
+fn print_table_array(vector: &mut Vec<(String, i32)>) {
+
+    for i in vector{
+        println!("SYSCALL: {:?} NUMBER OF TIMES: {}", i.0, i.1)
+    }
 }
